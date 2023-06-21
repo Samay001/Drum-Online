46ec8b82330d6e745f12f13c78891a14978d728b
Samay001 <121441938+Samay001@users.noreply.github.com>
2023-06-21 17:32:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,79 @@
+
+//detecting mouse press
+
+for(var i = 0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);  
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+
+//keyboard input and sound
+
+document.addEventListener("keydown",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+//common for both mouse and keyboard just a funtion using again and again so to make sound;
+
+function makeSound(key)
+{
+    switch(key)
+    {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "l":
+            var kick_bass = new Audio('sounds/kick-bass.mp3');
+            kick_bass.play();
+            break;
+
+        default:
+            console.log(buttonInnerHtml);
+    }
+}
+
+function buttonAnimation(key)
+{
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function()
+    {
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
+
